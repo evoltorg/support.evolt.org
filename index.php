8c796b5459b1894b73d8921eaefedba11d50217b
William Anderson <neuro@well.com>
2017-09-16 06:26:53 +0100

    catchup

---
+++ index.php
@@ -0,0 +1,133 @@
+<?php
+// support.evolt.org
+
+include "assets/inc/00-functions.inc";
+$site_name = "support.evolt.org";
+$page_title = "support.evolt.org";
+$html_title = "support.evolt.org";
+$short_title = "Support evolt.org";
+$page_type = "page";
+include "assets/inc/10-header.inc";
+include "assets/inc/20-message.inc";
+?>
+	<section id="content" class="full-width" role="main">
+
+
+<div class="white-box">
+	<div class="wrapper">
+		<div><p class="sub-heading-upper">Why Give?</p></div>
+		<div class="p100">
+<p>evolt.org is an all-volunteer organization that relies on the contributions of its members to operate and function. Thanks in part to the generous commitment of the web community, evolt.org provides renowned resources and information without advertisements.</p>
+
+<p>Here is a list of some of the services we provide:</p>
+
+<ul>
+<li> The Browser Archive<br />The largest browser archive available anywhere!</li>
+<li>Articles<br />Articles, tutorials and news items written by our members and available free to any visitor.</li>
+<li>List Servs<br />Numerous email lists to help disseminate knowledge to the web development community, and to help run evolt.org.</li>
+</ul>
+
+<p>The opportunities and services we provide are an increasing drain on evolt.org's limited finances. Bandwidth costs are our biggest expenditure; we currently transfer 250 GB/month, and that usage is growing. Hence, the need to ask for your help.</p>
+		</div>
+	</div>
+</div>
+<div class="grey-box">
+	<div class="wrapper">
+		<div><p class="sub-heading-upper">Help Support Evolt</p></div>
+		<div class="p100">
+			<p><strong>evolt.org</strong> is an <u>all-volunteer organization</u> that relies on the contributions of its members to operate and function. Thanks in part to the generous commitment of the web community, evolt.org provides renowned resources and information <u>without advertisements</u>.</p>
+			<p>While we are no longer accepting articles for the site, you can still contribute to the mailing lists and the browser archive, and you can also contribute financially to the costs of running the infrastructure to support those services.  <a href="/help_support_evolt">Find out more</a> about contributing to <strong>evolt.org</strong>.</p>
+		</div>
+		<div class="p33">
+			<p class="center">
+			<img src="http://evolt.org/front/assets/img/donate/paypal.png" alt="" width="32" height="32" style="vertical-align: middle" />
+			<img src="http://evolt.org/front/assets/img/donate/visa.png" alt="" width="32" height="32" style="vertical-align: middle" />
+			<img src="http://evolt.org/front/assets/img/donate/mastercard.png" alt="" width="32" height="32" style="vertical-align: middle" />
+			<img src="http://evolt.org/front/assets/img/donate/maestro.png" alt="" width="32" height="32" style="vertical-align: middle" />
+			<img src="http://evolt.org/front/assets/img/donate/amex.png" alt="" width="32" height="32" style="vertical-align: middle" />
+			<img src="http://evolt.org/front/assets/img/donate/wallet.png" alt="" width="32" height="32" style="vertical-align: middle" />
+			</p>
+		</div>
+		<div class="p33">
+			<p>
+			<a class="button" href="/help_support_evolt">Donate Now</a>
+		    </p>
+		</div>
+	</div>
+</div>
+
+<div class="white-box">
+	<div class="wrapper">
+		<div><p class="sub-heading-upper">Other Ways to Help</p></div>
+		<div class="p100">
+<h2>Knowledge Donations</h2>
+<p>We welcome many types of contributions, and recognise that not all of them will be tangible. We value the evolt.org community members who donate their time, skills, opinions and expertise to our various list servers, articles, and administration and management demands. While evolt.org's financial needs are great, there is no evolt without those contributions.</p>
+
+
+evolt.org is currently able to take donations online using PayPal, or by snail mail if you prefer. You can donate via PayPal in US dollars no matter what your home currency.
+
+Make an automated monthly donation
+
+Suport evolt.org every month using PayPal subscriptions.  (Please note: Due to PayPal transaction fees, we only offer the monthly online donation option in amounts of $10 US or more. If you'd like to give less, we appreciate your funding and encourage you to use the next method.)
+
+Support evolt.org with your monthly $10 donation
+Support evolt.org with your monthly $10 donation
+Support evolt.org with your monthly $15 donation
+Support evolt.org with your monthly $15 donation
+Support evolt.org with your monthly $20 donation
+Support evolt.org with your monthly $20 donation
+
+You can cancel your subscription at any time using your PayPal account. If you don't have a PayPal account, it's fast and easy to sign up during the registration process.
+
+Make a one-time donation
+
+We're sensitive to the fact that the exchange rate and cost of living is very different in different countries. All gifts, of all sizes, are most welcome and help evolt.org provide its services. You can make a one-time donation online or by snail mail in the amount of your choice:
+
+Support evolt.org with your donation of any amount
+Support evolt.org with your donation of any amount
+
+We also have a snail mail address for those who prefer to send a check, money order, or the like (please do not send cash):
+
+evolt.org
+Attn: Treasurer
+214A Glenbrook Road
+Bartonsville, PA 18321
+USA
+
+Please make checks payable to evolt.org. Also, please include your email or snail mail address, so we can send you a confirmation of receipt and our thanks.
+
+Evolt Merchandise
+
+You can buy all manner of evolt-branded merchandise at the evolt.org CafePress Store, including shirts, hats, bags and other fine goods.
+
+Affiliate donations
+
+By using the services of our affiliates, you make a donation to evolt.org at no additional cost to you. We will be adding to this list as new partnerships are formed:
+
+Amazon.com
+From technical books to leisure reading, CDs to DVDs, your Amazon purchases can help fund evolt.org everytime you start your shopping by clicking through from our site.
+
+If you'd like to become an evolt.org affiliate, or have an affiliate suggestion, please contact give@evolt.org.
+
+Google Ads in the Browser Archive
+
+We have begun running Google AdSense ads within the Browser Archive.  
+
+If you'd like, you can now advertise exclusively on evolt.org! You can use this link to sign up for a Google AdWords account and target ads to browsers.evolt.org.
+
+Placing an ad on evolt.org benefits both you (if you're trying to reach a high-traffic, high-quality audience of web designers, developers, hobbyists and professionals) and evolt.org too, because site-targeted ads generate more revenue for the care, feeding and hosting of evolt.org.
+
+If you see any ads appear which are inappropriate, or which appear to interfere with your access to the Browser Archive, please let us know as soon as possible.
+
+Thanks!
+
+If you have questions, please contact give@evolt.org. Regardless of what or how much you choose to contribute to evolt.org, it is sincerely appreciated.</p>
+
+		</div>
+	</div>
+</div>
+
+	</section>
+<?php
+include "assets/inc/99-footer.inc";
+?>
